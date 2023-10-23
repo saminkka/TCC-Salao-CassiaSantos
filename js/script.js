dc44e7e947ded324284b5b87ed983ee8621d269b
saminkkas <148074404+saminkkas@users.noreply.github.com>
2023-10-23 00:59:37 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,40 @@
-const headerEl = document.querySelector('.header')
-const navEl = document.querySelector('.nav')
-const mobileEl = document.querySelector('.mobile')
+const headerEl = document.querySelector('.header');
+const navEl = document.querySelector('.nav');
+const mobileEl = document.querySelector('.mobile');
 
+// Adicione uma variável para rastrear o estado do menu
+let isNavOpen = false;
 
 window.addEventListener('scroll', () => {
-    if(window.scrollY > 20){
-        headerEl.classList.add('header-scrolled')
-        navEl.classList.add('nav-scrolled')
-    } else if (window.scrollY <= 20){
-         headerEl.classList.remove('header-scrolled')
-         navEl.classList.remove('nav-scrolled')
+    if (window.scrollY > 20) {
+        headerEl.classList.add('header-scrolled');
+        navEl.classList.add('nav-scrolled');
+    } else if (window.scrollY <= 20) {
+        headerEl.classList.remove('header-scrolled');
+        navEl.classList.remove('nav-scrolled');
     }
-})
+});
 
-mobileEl.addEventListener('click', () =>{
-    navEl.classList.toggle("nav-open")
-    mobileEl.classList.toggle("mobile-open")
-})
+mobileEl.addEventListener('click', () => {
+    // Alterne o estado do menu
+    isNavOpen = !isNavOpen;
+
+    if (isNavOpen) {
+        navEl.classList.add("nav-open");
+    } else {
+        navEl.classList.remove("nav-open");
+    }
+});
+
+// Feche o menu ao clicar em um link no menu
+const navLinks = document.querySelectorAll('.nav_link');
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        navEl.classList.remove('nav-open');
+        mobileEl.classList.remove('mobile-open');
+        isNavOpen = false;
+    });
+});
 
 const cards = document.querySelectorAll('.card');
 
@@ -99,3 +117,12 @@ carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 wrapper.addEventListener("mouseleave", autoPlay);
 
+//SLIDER CUIDADOS
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.slider-item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.slider-item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
