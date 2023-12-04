8f0f0817f5fc7639d41b9559ba726f00dc0dde29
saminkkas <148074404+saminkkas@users.noreply.github.com>
2023-12-04 08:12:20 -0300

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,41 +1,12 @@
-const headerEl = document.querySelector('.header');
-const navEl = document.querySelector('.nav');
-const mobileEl = document.querySelector('.mobile');
-
-// Adicione uma variável para rastrear o estado do menu
-let isNavOpen = false;
+const headerEl = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 20) {
         headerEl.classList.add('header-scrolled');
-        navEl.classList.add('nav-scrolled');
     } else if (window.scrollY <= 20) {
         headerEl.classList.remove('header-scrolled');
-        navEl.classList.remove('nav-scrolled');
     }
 });
-
-mobileEl.addEventListener('click', () => {
-    // Alterne o estado do menu
-    isNavOpen = !isNavOpen;
-
-    if (isNavOpen) {
-        navEl.classList.add("nav-open");
-    } else {
-        navEl.classList.remove("nav-open");
-    }
-});
-
-// Feche o menu ao clicar em um link no menu
-const navLinks = document.querySelectorAll('.nav_link');
-navLinks.forEach((link) => {
-    link.addEventListener('click', () => {
-        navEl.classList.remove('nav-open');
-        mobileEl.classList.remove('mobile-open');
-        isNavOpen = false;
-    });
-});
-
 
 //SLIDER CUIDADOS
 document.getElementById('next').onclick = function(){
